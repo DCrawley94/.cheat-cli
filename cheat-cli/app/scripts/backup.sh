616cdcf83525e72698269c87f5f9a67b23119dd3
DCrawley94 <duncan.crawley@gmail.com>
2022-05-16 16:51:27 +0100

    remove unecessary arg

--- cheat-cli/app/scripts/backup.sh
+++ cheat-cli/app/scripts/backup.sh
@@ -2,8 +2,6 @@
 
 # PURPOSE: RSYNC COMMAND TO BACKUP CHEATSHEET DIRECTORIES
 
-# ARG (OPTIONAL) = PATH TO BACKUP DIRECTORY (DEFAULT TO $HOME/.cheat-cli/cheatsheet-backup
-
 # --- COLOURS ---
 
 GREEN_BOLD='\033[1;32m'
@@ -14,11 +12,6 @@ NORMAL='\033[0m'
 pathToCheatSheets="$HOME/.cheat-cli/cheatsheets/"
 backupDir="$HOME/.cheat-cli/cheatsheet-backup"
 
-# TO DO: Error handle if given argument doesn't match file path regex
-
 # --- SCRIPT ---
-if [ "$#" = 1 ]; then
-  backupDir="$1"
-fi
 
 rsync -av "$pathToCheatSheets" "$backupDir" &>/dev/null && echo -e "Successfully backed up notes to $GREEN_BOLD$backupDir$NORMAL 🤟"
