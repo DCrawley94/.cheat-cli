738a02cb496da415f98431510a317c6b14fa2b45
DCrawley94 <76622730+DCrawley94@users.noreply.github.com>
2022-05-16 22:00:28 +0100

    feature: browse notes

--- cheat-cli/app/index.js
+++ cheat-cli/app/index.js
@@ -1,3 +1,3 @@
 exports.addNote = require('./lib/programs/add-note');
-exports.findNotes = require('./lib/programs/find-note');
+exports.browseNotes = require('./lib/programs/browse-note');
 exports.backupNotes = require('./lib/programs/backup-notes');
