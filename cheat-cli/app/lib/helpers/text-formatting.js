94201e923e7de1d66a5a79789945e94e3adfed9c
DCrawley94 <duncan.crawley@gmail.com>
2022-05-19 23:55:17 +0100

    add more terminal colour styles

--- cheat-cli/app/lib/helpers/text-formatting.js
+++ cheat-cli/app/lib/helpers/text-formatting.js
@@ -1,11 +1,39 @@
 // PRINT FANCY MESSAGES 🧑‍🎨 💅
 
 const terminalStyles = {
-	lightBlueBold: '\n\x1b[36m\x1b[1m',
-	darkBlueBold: '\n\x1b[34m\x1b[1m',
-	yellowBold: '\x1b[1m\x1b[33m',
-	reset: '\x1b[0m'
+	reset: '\x1b[0m',
+	bright: '\x1b[1m',
+	dim: '\x1b[2m',
+	underscore: '\x1b[4m',
+	blink: '\x1b[5m',
+	reverse: '\x1b[7m',
+	hidden: '\x1b[8m',
+
+	black: '\x1b[30m',
+	red: '\x1b[31m',
+	green: '\x1b[32m',
+	yellow: '\x1b[33m',
+	blue: '\x1b[34m',
+	magenta: '\x1b[35m',
+	cyan: '\x1b[36m',
+	white: '\x1b[37m',
+
+	BGblack: '\x1b[40m',
+	BGred: '\x1b[41m',
+	BGgreen: '\x1b[42m',
+	BGyellow: '\x1b[43m',
+	BGblue: '\x1b[44m',
+	BGmagenta: '\x1b[45m',
+	BGcyan: '\x1b[46m',
+	BGwhite: '\x1b[47m'
 };
+
+//{
+// 	lightBlueBold: '\n\x1b[36m\x1b[1m',
+// 	darkBlueBold: '\n\x1b[34m\x1b[1m',
+// 	yellowBold: '\x1b[1m\x1b[33m',
+// 	reset: '\x1b[0m'
+// };
 
 // TEXT FORMATTING
 
@@ -34,17 +62,19 @@ function kebabCase(string) {
 
 function printNotes(notes, techName) {
 	console.log(
-		`${terminalStyles.lightBlueBold}--- ${formatFileName(techName)} ---`
+		`\n${terminalStyles.cyan}${terminalStyles.bright}--- ${formatFileName(
+			techName
+		)} ---`
 	);
 	for (const title in notes) {
-		console.log(`${terminalStyles.darkBlueBold}  ${title}`);
+		console.log(`\n${terminalStyles.blue}  ${title}`);
 		console.log(`   ${terminalStyles.reset} ~ ${notes[title]}\n`);
 	}
 }
 
 function printErrorMessage(message) {
 	console.log(
-		` - ${terminalStyles.yellowBold}${message}${terminalStyles.reset} - `
+		` - ${terminalStyles.yellow}${message}${terminalStyles.reset} - `
 	);
 }
 
